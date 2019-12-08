708044eb32f0da1a682996eeb0ca67d53f98e75b
Erkki Lepre <erkki.lepre@f-secure.com>
2019-12-08 18:53:49 +0200

    Added unit tests for Lautapelit

--- backend/parsers/lautapelit.js
+++ backend/parsers/lautapelit.js
@@ -1,27 +1,24 @@
 const rp = require('request-promise')
 const request = require('request')
 
-const baseUrl = 'http://www.lautapelit.fi/sysNet/getProductsJSON/getProductsJSON.aspx?sua=1&lang=1&reID=1&sanaHaku='
+const baseUrl = 'https://www.lautapelit.fi/sysNet/getProductsJSON/getProductsJSON.aspx?sua=1&lang=1&reID=1&sanaHaku='
 
 async function query(querystring){
     let url = 'https://www.lautapelit.fi/sysNet/getProductsJSON/getProductsJSON.aspx?sua=1&lang=1&reID=1&sanaHaku=dungeon%20petz';
     let data = await rp(url, {json: true, gzip: true})
     let out = [];
-
-    data.forEach(el => {
-        //let availabilityUrl = `http://www.lautapelit.fi/getVarastosaldoJSON.asp?sua=1&lang=1&s=${el.SivuID}&tID=${el.TuoteID}`
-        //let availabilityData = await rp(url, {json: true})
-
+    for (const el of data) {
+        let availabilityUrl = `https://www.lautapelit.fi/getVarastosaldoJSON.asp?sua=1&lang=1&s=${el.SivuID}&tID=${el.TuoteID}`
+        let availabilityData = await rp(availabilityUrl, {json: true, gzip: true})
         out.push({
             name: el.Tuote,
             imageUrl: el.Kuva,
             price: el.Hinta,
-            //available: availabilityData.Mera.VarastoSaldo > 0,
-            available: 1 > 0,
+            available: availabilityData.Mera[0].VarastoSaldo > 0,
             itemUrl: 'https://www.fantasiapelit.com/',
             currency: '€',
         })
-    });
+    }
 
     return out;
 }
--- backend/test.js
+++ backend/test.js
@@ -2,11 +2,14 @@ const assert = require('assert');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('./app');
-//const lautapelit = require('./lautapelit');
+const lautapelit = require('./parsers/lautapelit');
 const should = chai.should();
+const nock = require('nock')
+const mockResponses = require('./mock_responses')
+
 chai.use(chaiHttp);
 
-const RUN_INTEGRATION_TESTS = true;
+const RUN_INTEGRATION_TESTS = false;
 
 describe("Handlers", function(){
     it("should return a list", done=>{
@@ -17,6 +20,31 @@ describe("Handlers", function(){
                 res.body.should.have.length(2);
                 done();
             })
+    })
+})
+
+describe("Lautapelit.fi", function(){
+    it("should process the reponse", async function(){
+        let qs = 'dungeon%20petz'
+        nock('https://www.lautapelit.fi')
+        .get('/sysNet/getProductsJSON/getProductsJSON.aspx?sua=1&lang=1&reID=1&sanaHaku=' + qs)
+        .reply(200, mockResponses.RESPONSE_LAUTAPELIT)
+        .get('/getVarastosaldoJSON.asp?sua=1&lang=1&s=16588&tID=15613')
+        .reply(200, mockResponses.RESPONSE_LAUTAPELIT_AVAILABILITY_AVAILABLE)
+        .get('/getVarastosaldoJSON.asp?sua=1&lang=1&s=21297&tID=20227')
+        .reply(200, mockResponses.RESPONSE_LAUTAPELIT_AVAILABILITY_NOT_AVAILABLE)
+
+        res = await lautapelit(qs)
+        item = res[0]
+        item.should.have.property('name', 'Dungeon Petz (ENG)');
+        item.should.have.property('imageUrl');
+        item.should.have.property('price');
+        item.should.have.property('available', true);
+        item.should.have.property('itemUrl');
+        item.should.have.property('currency');
+
+        item = res[1]
+        item.should.have.property('available', false);
     })
 })
 
@@ -34,12 +62,15 @@ describe("Intergration", function(){
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.shop.should.equal('lautapelit');
-                    res.body.data[0].should.have.property('name');
-                    res.body.data[0].should.have.property('imageUrl');
-                    res.body.data[0].should.have.property('price');
-                    res.body.data[0].should.have.property('available');
-                    res.body.data[0].should.have.property('itemUrl');
-                    res.body.data[0].should.have.property('currency');
+
+                    let item = res.body.data[0]
+                    //item.should.have.keys('name', 'imageUrl');
+                    item.should.have.property('name');
+                    item.should.have.property('imageUrl');
+                    item.should.have.property('price');
+                    item.should.have.property('available');
+                    item.should.have.property('itemUrl');
+                    item.should.have.property('currency');
                     done();
                 })
         })
