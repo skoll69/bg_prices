46bd19e96d3c0d55eaa821ade492982e15087e2e
Erkki Lepre <erkki.lepre@iki.fi>
2022-11-20 09:56:47 +0200

    Removed obsolete mock response

--- mock_responses.js
+++ mock_responses.js
@@ -726,48 +726,6 @@ module.exports = {
 	</html>
 	`,
 
-    RESPONSE_LAUTAPELIT_OBS: [
-    {
-      Tuote: 'Dungeon Petz (ENG)',
-      Koodi: '8594156310158',
-      EAN: 'LPDPETZ',
-      ALV: '24',
-      Kuva: 'images/tuotekuvat/kuva100/2014/dungeon-petz-14.jpg',
-      'Myyntiyksikkö': 'kpl',
-      SivuID: 16588,
-      TuoteID: 15613,
-      Myynnissa: 1,
-      Kapasiteetit: 0,
-      'MyyntiyksikköID': 15644,
-      Hinta: 45,
-      Uutuus: false,
-      Tiedustelu: false,
-      isAlennusTuote: false,
-      isMyynnissa: true,
-      showHinta: true
-    },
-    {
-      Tuote: 'Dungeon Petz: Dark Alleys (ENG)',
-      Kuvaus: 'Bringing you more than just new pets, cages and magic items, the Dungeon Petz: Dark Alleys has a whole new district in the town.',
-      Koodi: '0681706170930',
-      EAN: '8594156310240',
-      ALV: '24',
-      Kuva: 'kuva100/lautapelit/Dungeon-petz-dark-alleys.jpg',
-      'Myyntiyksikkö': 'kpl',
-      SivuID: 21297,
-      TuoteID: 20227,
-      Myynnissa: 1,
-      Kapasiteetit: 0,
-      'MyyntiyksikköID': 20267,
-      Hinta: 29.9,
-      Uutuus: false,
-      Tiedustelu: false,
-      isAlennusTuote: false,
-      isMyynnissa: true,
-      showHinta: true
-    }
-  ],
-
   RESPONSE_LAUTAPELIT_AVAILABILITY_AVAILABLE: {
     Mera: [
       {
